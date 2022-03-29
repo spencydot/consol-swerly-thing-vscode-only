20a3aa6f7cf150fca13821313889305e7ecc13d9
spencydot <spencerjtrott@gmail.com>
2022-03-29 15:57:29 +0100

    Create sdfsdf.py

---
+++ sdfsdf.py
@@ -0,0 +1,239 @@
+import time 
+import datetime
+
+off =  '🟥🟥'
+offT = '🟦🟦'
+
+on = "⬜"
+
+Px = on
+Px1 = on
+Px2 = on
+Px3 = on
+Px4 = on
+Px5 = on
+Px6 = on
+Px7 = on                        
+Px8 = on 
+Px9 = on
+Px10 = on
+Px11 = on
+Px12 = on
+
+DATA = ""
+
+#Px,Px1,Px2,Px3,Px4,"\n",
+Delay = 0.05
+
+UP = 0.03
+DOWN = 0.03
+while True:
+    #on = "$"
+    Px = off
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+    )
+    Px = on
+    
+    Px1 = off
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px1 = on
+    
+    time.sleep(Delay)
+    Px2 = off
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px2 = on
+    
+    Px3 = off
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px3 = on
+    
+    Px4 = off
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px4 = on
+    
+    Px5 = off
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px5 = on
+    
+    Px6 = off
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px6 = on
+    
+    Px7 = off
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px7 = on
+    
+    Px8 = off
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px8 = on
+    
+    Px9 = off
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px9 = on
+    
+    Px10 = off
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px10 = on
+    
+    Px11 = off
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px11 = on
+    
+    Px12 = off
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px12 = on
+    Delay = UP
+##############################################################################
+    #on = '@'
+    Px12 = offT
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ",Delay,datetime.datetime.now(),DATA,
+    )
+    Px12 = on
+    
+    Px11 = offT
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px11 = on
+    
+    time.sleep(Delay)
+    Px10 = offT
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px10 = on
+    
+    Px9 = offT
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px9 = on
+    
+    Px8 = offT
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px8 = on
+    
+    Px7 = offT
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px7 = on
+    
+    Px6 = offT
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px6 = on
+    
+    Px5 = offT
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px5 = on
+    
+    Px4 = offT
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px4 = on
+    
+    Px3 = offT
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px3 = on
+    
+    Px2 = offT
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px2 = on
+    
+    Px1 = offT
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px1 = on
+    
+    Px = offT
+    time.sleep(Delay)
+    print(
+        Px,Px1,Px2,Px3,Px4,Px5,Px6,Px7,Px8,Px9,Px10,Px11,Px12, "   ", Delay,datetime.datetime.now(),DATA,
+
+    )
+    Px = on
+    Delay = DOWN
